50ea27276f25cab19b2797e6a592a988f70a2556
Sarah Jones <sarah@ringrevenue.com>
2012-10-24 19:55:32 -0700

    Failing test

--- jellybelly/jellybelly.rb
+++ jellybelly/jellybelly.rb
@@ -1,6 +1,20 @@
 class JellyBelly
   def get_string(number)
- 
+    if number % 3 == 0
+      if number % 5 == 0
+        "#{number} Jelly Belly!"
+      else
+        "#{number} Jelly"
+      end
+    elsif number % 5 == 0
+      "#{number} Belly"
+    else
+      "#{number}"
+    end
     
+  end
+  
+  def print_all number
+  
   end
 end
--- jellybelly/jellybelly_test.rb
+++ jellybelly/jellybelly_test.rb
@@ -21,7 +21,7 @@ class JellyBellyTest < Test::Unit::TestCase
   def test_should_print_jellybelly_when_number_is_divisible_by_3_and_5
     jellybelly = JellyBelly.new
 
-    assert_equal "15 JellyBelly!", jellybelly.get_string(15) 
+    assert_equal "15 Jelly Belly!", jellybelly.get_string(15) 
   end
 
   def test_should_print_the_number
@@ -32,10 +32,11 @@ class JellyBellyTest < Test::Unit::TestCase
 
   def test_should_print_out_all_numbers_less_than_the_parameter
     jellybelly = JellyBelly.new
-    output = []
-    IO.any_instance.expects(:puts).with { |s| output << s }
+    
+    [1..4].each do |i|
+      jellybelly.expects(:get_string).with(i)
+    end
     out = jellybelly.print_all(4) 
-   
-    assert_equal ["1", "2", "3 Jelly", "4"]   
+  
   end
 end
